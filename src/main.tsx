b96e38e10cf29ad5fff969361e15a7faca4158bc
Julien PICART <julienpicart01@gmail.com>
2025-06-27 10:02:47 +0200

    createRoot

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// Import necessary modules from React and React Router
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./index.css";
+
+// Find the root element in the HTML document
+const rootElement = document.getElementById("root");
+if (rootElement == null) {
+	throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
+}
+
+// Render the app inside the root element
+createRoot(rootElement).render(
+	<StrictMode>
+		<AuthProvider>
+			<RouterProvider router={router} />
+		</AuthProvider>
+	</StrictMode>,
+);
